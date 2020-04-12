d733229ecb8d70bc0226ce76eae458dc6d70eef5
unknown <oske.design@gmail.com>
2020-04-13 03:44:38 +0500

    new client id

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ const client = new MongoClient(url, { useUnifiedTopology: true });
 const app = express();
 
 const { OAuth2Client } = require('google-auth-library');
-const CLIENT_ID = '541301727496-586jhpqk4bsojhr1n4onrtrr8iojv2t7.apps.googleusercontent.com';
-const clien2t = new OAuth2Client('541301727496-586jhpqk4bsojhr1n4onrtrr8iojv2t7.apps.googleusercontent.com');
+const CLIENT_ID = '541301727496-8v8snacmlj9pub2ojvi3ksbkepbl4okl.apps.googleusercontent.com';
+const clien2t = new OAuth2Client(CLIENT_ID);
 
 app.use(cors());
 app.use(express.json());
